5b1e4d37f2589e26e579c52e1aab814c144be787
Ignacio Revainera <ignacio.revainera@hotmail.com>
2026-05-21 17:28:18 -0300

    test: agregar MainTest para pruebas manuales

---
+++ ProyectoFestival/test/MainTest.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Clase de prueba mínima para ejecutar pruebas manuales o unitarias.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("MainTest: punto de entrada de pruebas.");
+    }
+}
